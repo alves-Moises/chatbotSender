bcb3937798dd56c6892dd0f70f3382f00852f8c0
alves-Moises <moisesinho_ra@hotmail.com>
2024-10-31 02:23:57 -0300

    refactor: app server features

--- src/server/app.js
+++ src/server/app.js
@@ -1,15 +1,15 @@
 const chalk = require("chalk");
 
-
 const app = require("./routes/api.js")
 const port = 8000
+
 
 // ===== SERVER INTIALIZATING... =====
 app.listen(port, () => {
     console.log(
-        `${chalk.yellow("SERVIDOR")} ${chalk.green("OK")}! }`
+        `${chalk.yellow("SERVIDOR")} ${chalk.green("OK")}! `
     )
-  
+
     console.log(`PORT: ${chalk.bgRed(`${port}  `)}`)
 })
 
