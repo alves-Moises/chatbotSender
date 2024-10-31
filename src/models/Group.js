01f1b7061c3b90cdd9ad97b6c67b741b146ac3d7
alves-Moises <moisesinho_ra@hotmail.com>
2024-10-31 02:31:02 -0300

    fix: Group create method

--- src/models/Group.js
+++ src/models/Group.js
@@ -2,11 +2,13 @@ const db = require('../db/database')
 
 const Group = {
     //table: groups
-    async create(group) {
-        console.log(group)
-        const query = "INSERT INTO groups VALUES ?";
-        await db.execute(query, group);
-        return group;
+    async create(groupData) {
+        let name = groupData.name 
+        let group_id = groupData.group_id
+        const query = `INSERT INTO groups (name, group_id) VALUES ('${name}', '${group_id}')`;
+
+        await db.execute(query);
+        return groupData;
     },
 
     async list(){
@@ -19,8 +21,6 @@ const Group = {
         const query = "DELETE FROM groups WHERE id = ?"
         await db.execute(query, [ID])
     }
-
-
 }
-
+    
 module.exports = Group
