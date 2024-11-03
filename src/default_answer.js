38ba54b8ad219c8373ee0791b3caff8428f00265
alves-Moises <moisesinho_ra@hotmail.com>
2024-11-02 23:40:26 -0300

    new: messages default

---
+++ src/default_answer.js
@@ -0,0 +1,58 @@
+
+
+const Help = () => { 
+    return  `
+        *prefix:* ?
+        *Commands:*
+
+        help: show this list
+        addgroup <type>: add a group to my db
+        
+        sbyt <type>: search groups by type
+        
+            *_type: vendas (default), dev, eletrica, filme, meu
+
+    `
+}
+const ITMessage = () => {
+    return `
+        Alves Informática
+    
+        A sua solução em tecnologia! 🚀
+        
+        Oferecemos serviços de qualidade em:
+        
+        🖥️💻 Manutenção de computadores e notebooks
+        - Formatação, atualização e instalação de softwares
+        - Recuperação de arquivos perdidos ou deletados
+        - Troca de pasta térmica e limpeza interna
+        - Reparo de fontes e circuitos elétricos
+        - Configuração de impressoras e redes
+        
+        🕹️💽 Manutenção de video games
+        - Instalação do OPL para rodar jogos de PS2 via pen drive
+        - Troca de pasta térmica e limpeza interna
+        - Reparo de circuitos eletrônicos
+        - Instalação e atualização do Recalbox para jogar clássicos retrô
+        - Montagem e configuração de Raspberry Pi
+        
+        📱📱 Manutenção de celulares
+        - Troca de tela e aplicação de película
+        - Reparo de botões, alto-falantes e microfones
+        - Limpeza de conector de carga e entrada de fone
+        - Desbloqueio e restauração de sistema
+        
+        🌐🌐 Desenvolvimento de sites para pequenas empresas
+        - Criação de sites responsivos, modernos e otimizados para o Google
+        - Hospedagem, domínio e suporte técnico inclusos a combinar
+        
+        
+        Entre em contato pelo chat ou pelo WhatsApp e faça seu orçamento sem compromisso. Aceitamos PIX 📲
+        
+        https://alves-informatica.com/
+        Alves Informática - A sua solução em tecnologia! 🚀
+        
+    `
+}
+
+module.exports = { Help, ITMessage }
