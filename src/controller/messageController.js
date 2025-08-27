6f09eae7e1fadb816ffe7d1a60190666bbfeaaa4
alves-Moises <moisesinho_ra@hotmail.com>
2025-08-27 02:03:23 -0300

    new: message controller

---
+++ src/controller/messageController.js
@@ -0,0 +1,44 @@
+const client = require("../clientStart")
+
+const chalk = require("chalk")
+const yellow = chalk.yellow
+const green = chalk.green
+
+
+
+const  sendGroupsMessage = async (type, text) => {
+    let groups = await findGroupsByType(type)
+    let timeStart = Date.now()
+    console.log(yellow(`Enviando ${type} message...`))
+    for(let i = 0; i < groups.length; i++){
+        console.log(`${i}: ` + yellow(groups[i].name))
+        var timeStartMsg = Date.now()
+        await client.sendMessage(
+            groups[i].id, 
+            sendTexts[text]()
+        ).then(
+            () => {
+                var timeEnd = Date.now()
+                var timeTotalMsg = (timeEnd - timeStartMsg) / 1000
+                console.log(
+                    green("Message sent!"), 
+                    yellow(timeTotalMsg), "secconds."
+                )
+            }
+        )
+    }
+    let timeFinal = Date.now()
+    let totalTime = (timeStart - timeFinal) / 1000
+        console.log(
+            green(`
+                ===========
+                Succes! ${groups.length} groups
+                ${totalTime} secs
+                ============
+            `)
+
+
+        )
+}
+
+module.exports = {sendGroupsMessage}
