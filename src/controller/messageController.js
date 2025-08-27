f17e01d78ee8e9114ad19917761ee4ef134ed4dc
alves-Moises <moisesinho_ra@hotmail.com>
2025-08-27 04:35:17 -0300

    refactor message controller

--- src/controller/messageController.js
+++ src/controller/messageController.js
@@ -1,44 +1,43 @@
-const client = require("../clientStart")
-
 const chalk = require("chalk")
+const client = require("../clientStart")
+const prefix = "?"
+
 const yellow = chalk.yellow
 const green = chalk.green
+const red = chalk.red
 
+client.on("message", async (msg) => {
+    let msgLower = await msg.body.toLocaleLowerCase().trim()
+    let from = await msg.from 
+    let user = await msg.getContact()
+    let user_name = await user.pushname
+    let chat = await msg.getChat()
+    
+    if(msgLower == prefix + "ping"){
+        msg.reply('pong')
+        console.log(`pong... ${chalk.yellow(`${user_name}`)}`)
+    }
 
+    // just for debbuging...
+    try{
+        let data = new Date(msg.timestamp * 1000);
 
-const  sendGroupsMessage = async (type, text) => {
-    let groups = await findGroupsByType(type)
-    let timeStart = Date.now()
-    console.log(yellow(`Enviando ${type} message...`))
-    for(let i = 0; i < groups.length; i++){
-        console.log(`${i}: ` + yellow(groups[i].name))
-        var timeStartMsg = Date.now()
-        await client.sendMessage(
-            groups[i].id, 
-            sendTexts[text]()
-        ).then(
-            () => {
-                var timeEnd = Date.now()
-                var timeTotalMsg = (timeEnd - timeStartMsg) / 1000
-                console.log(
-                    green("Message sent!"), 
-                    yellow(timeTotalMsg), "secconds."
+        let dataFormatada = data.toLocaleDateString('pt-BR', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit'
+        });
+        console.log(yellow("\nMensagem recebida..."))
+        await console.log(`${green(dataFormatada)} || ${yellow(chat.name)} || \n${msgLower.slice(0, 100)}`)
+    }catch{
+        console.log(
+            `${red(
+                "Erro ao receber mensagem"
                 )
-            }
+            }`
         )
     }
-    let timeFinal = Date.now()
-    let totalTime = (timeStart - timeFinal) / 1000
-        console.log(
-            green(`
-                ===========
-                Succes! ${groups.length} groups
-                ${totalTime} secs
-                ============
-            `)
-
-
-        )
-}
-
-module.exports = {sendGroupsMessage}
+})
