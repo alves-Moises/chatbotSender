8652b2fdd52cac06fe4d24e89bc035c271ad2c40
alves-Moises <moisesinho_ra@hotmail.com>
2025-08-28 04:17:59 -0300

    refactor style tree

--- public/js/script.js
+++
@@ -1,7 +0,0 @@
-function sendCustomADS(){
-    let msg = document.getElementById("msg").value
-    window.location.href = `${window.location.href}/custom-message/${msg}`;
-}
-
-customMessageBtn = document.getElementById("custom_msg_send")
-customMessageBtn.addEventListener("click", sendCustomADS)
